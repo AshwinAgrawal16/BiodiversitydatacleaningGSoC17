f804a22c02c8e45a6026b0c0c36814fd63776912
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2017-03-06 00:41:44 +0530

    Update DateTest.R
    Optimized

--- DateTest.R
+++ DateTest.R
@@ -6,28 +6,42 @@
 
 library(rgbif)
 
-CheckDateRecords<-function(x,size=2000,count=0,refData=NULL){
+#CheckDateRecords<-function(x,size=2000,count=0,refData=NULL){
+#
+#                  refData<-as.data.frame(x$data)
+#                   #Adding the flag column with initial entries as 0
+#                   redData<-cbind(flag = 0,refData)
+#                   if (("eventDate" %in% names(redData)) ){
+#                   for(i in 1:size){
+#
+#                   if(!is.na(refData[i,"eventDate"]) ){
+#                   count=count+1
+#                   refData[i,"flag"]=1    # Flagging the records
+#                                        }
+#                                   }
+#
+#                                            }
+#                   #Number of records flagged
+#                   cat(sprintf("Number of flagged  records from the data \n"))
+#                   print(count)
+#                   #returns a array of flagged records with value 1 if flagged otherwise 0
+#                   return(refData[,ncol(refData)])
+#}
 
-                   refData<-as.data.frame(x$data)
-                   #Adding the flag column with initial entries as 0
-                   redData<-cbind(flag = 0,refData)
-                   if (("eventDate" %in% names(redData)) ){
-                   for(i in 1:size){
-
-                   if(!is.na(refData[i,"eventDate"]) ){
-                   count=count+1
-                   refData[i,"flag"]=1    # Flagging the records
-                                        }
-                                   }
-
-                                            }
-                   #Number of records flagged
-                   cat(sprintf("Number of flagged  records from the data \n"))
-                   print(count)
-                   #returns a array of flagged records with value 1 if flagged otherwise 0
-                   return(refData[,ncol(refData)])
-}
-
+CheckDateRecords<-function(data){
+                    data$flag <- "Good"
+                    #Checking the bad indexes 
+                    badindex <- which(is.na(data$day)|is.na(data$month)|is.na(data$year))
+  
+                    if(length(badindex)>0){
+                    data[badindex,]$flag <- "Bad"   #flagging the bad indexes
+                    }
+  
+                    print(head(data[, c("day", "month", "year", "flag")]))
+  
+  
+  
+  }
 
 
 ##EXAMPLE:
@@ -37,6 +51,6 @@ CheckDateRecords<-function(x,size=2000,count=0,refData=NULL){
 size=2000
 #Class key=359 corresponds to mammals
 res <- occ_search(classKey = 359, limit = size)
-
+res<-res$data
 #Function call
 CheckDateRecords(res)
