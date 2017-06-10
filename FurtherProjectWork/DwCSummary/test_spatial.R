13c83fc51d1994ba9a626cf8c823757763f4ade3
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2017-06-10 20:33:38 +0530

    DwC Summary for spatial fields

--- FurtherProjectWork/DwCSummary/test_spatial.R
+++ FurtherProjectWork/DwCSummary/test_spatial.R
@@ -1,22 +1,182 @@
-X<-read.csv(file = "C:\\yourfile.csv")
-X1<-X[,1:4]
-X1<-X1[,-2]
-dim(X1)
-X1<-unique(X1)
+DwC_Summary_spatial<-function(X,DECIMALLATITUDE=NULL,
+                              DECIMALLONGITUDE=NULL,
+                              COORDINATEPRECISION=NULL,
+                              COORDINATEUNCERTAINITYINMETERS=NULL,
+                              COUNTRYCODE=NULL){
+  
+  
+  
+  if(nrow(X)==0){
+    cat(paste("The data set is empty"))
+  }
+  else{
+  X<-subset(X,select=c(decimalLatitude,decimalLongitude,coordinatePrecision,
+                       coordinateUncertaintyInMeters))
+  
+  
+  mapgilbert <- get_map(location = c(lon = mean(X$decimalLongitude), lat = mean(X$decimalLatitude)), zoom = 4,
+                        maptype = "terrain", scale = 2)
+  
+  # Normal plotting
+  ggmap(mapgilbert) +
+    geom_point(data = X, aes(x = decimalLongitude, y = decimalLatitude, alpha = 0.8, size=5,colour= 'red',fill="red" ),shape=20) +
+    guides(fill=FALSE, alpha=FALSE, size=FALSE)
+  
+  
+  # Plotting with coordinatePrecision
+  ggmap(mapgilbert) +
+    geom_point(data = X, aes(x = decimalLongitude, y = decimalLatitude, alpha = 0.5, size =coordinatePrecision, colour= coordinatePrecision ),shape=20)
+    
+  
+  #Plotting with coordinateuncertainityinmeters
+  ggmap(mapgilbert) +
+    geom_point(data = X, aes(x = decimalLongitude, y = decimalLatitude, alpha = 0.5, size =coordinateUncertaintyInMeters, colour= coordinateUncertaintyInMeters ),shape=20)
+    
+  
+  
+  
+  cat(paste(" Bounding box of records ",min(X$decimalLatitude,na.rm=T),",",min(X$decimalLongitude,na.rm=T),
+            "------",max(X$decimalLatitude,na.rm=T),",",max(X$decimalLongitude,na.rm=T),"\n"))
+  
+  
+  mean_centerX <- mean(X[,1])
+  mean_centerY <- mean(X[,2])
+  
+  cat(paste("Centre of the data set",mean_centerX,",",mean_centerY))
+  
+  standard_deviationX <- sd(X[,1])
+  standard_deviationY <- sd(X[,2])
+  standard_distance <- sqrt(sum(((X[,1]-mean_centerX)^2+(X[,2]-mean_centerY)^2))/(nrow(X)))
+  
+  cat(paste("Standard deviation in coordinates", standard_deviationX,"," ,standard_deviationY ))
+  cat(paste("Standard distnace of points", standard_distance))
+  
+  cat(paste("Area of the ploygon formed by the coordinates",pracma::polyarea(X[,1],X[,2])))
+  
+  
+  #Inter Quartile range
+  ll<-as.double(X[,1])
+  iqr1<-IQR(ll,type = 4)
+  ll<-as.double(X[,2])
+  iqr2<-IQR(ll,type=4)
+  
+  
+  
+  # Plotting density circle
+  X1<-unique(X[,2:1])
+  mc <- apply(X1, 2, mean)
 
-# creating a sample data.frame with your lat/lon points
-lon <- c(-38.31,-35.5)
-lat <- c(40.96, 37.5)
-df <- as.data.frame(cbind(lon,lat))
-df<-X1
-View(df)
-X1$
-  # getting the map
-  mapgilbert <- get_map(location = c(lon = mean(df$decimalLongitude), lat = mean(df$decimalLatitude)), zoom = 4,
-                        maptype = "satellite", scale = 2)
+  # standard distance
+  sd <- sqrt(sum((X1[,1] - mc[1])^2 + (X1[,2] - mc[2])^2) / nrow(X1))
+  
+  
+  # visualising the data based on measures of central tendency
+  plot(X1, col='light blue')
+  points(X1, cex=.5)
+  points(cbind(mc[1], mc[2]), pch='*', col='red', cex=5)
+  
+  # make a circle
+  bearing <- 1:360 * pi/180
+  cx <- mc[1] + sd * cos(bearing)
+  cy <- mc[2] + sd * sin(bearing)
+  circle <- cbind(cx, cy)
+  lines(circle, col='red', lwd=2)
+  
+  
+  # nearest neighbour index
+  
+  sp.mydata <- X1
+  coordinates(sp.mydata) <- ~decimalLongitude+decimalLatitude
+  
+  NNI<-spatialEco::nni(sp.mydata)
+  
+  cat(paste("The expected nearest neighbour distance is",NNI$expected.mean.distance))
+  cat(paste("The observed nearest neighbour distance is",NNI$observed.mean.distance))
+  
+  if(NNI$NNI>1){
+    cat(paste("The nearest neib=gbhour index is greater that one-",NNI$NNI," therefore the data is dispersed"))
+  }
+  
+  if(NNI$NNI<1){
+    cat(paste("The nearest neib=gbhour index is greater that one-",NNI$NNI," therefore the data is clustered"))
+  }
+  
+  if((NNI$z.score<-2.58 || NNI$NNI>2.58) && NNI$p<0.01){
+    cat(paste("The null hypothesis can be rejected and data is significant and not random"))
+  }
+  else{
+    cat(paste("The null hypothesis cannot be rejected and data can be random"))
+  }
+  
+  #Kernel density
+  sp.mydata <- X
+  coordinates(sp.mydata) <- ~decimalLongitude+decimalLatitude
+  nni(sp.mydata)
+  nni(sp.mydata)
+  View(coordinates(sp.mydata))
+  kde<-spatialEco::kde2D(sp.mydata)
+  
+  cat(paste("The plot for kernel density"))
+  plot(kde$kde)
+  plot(sp.mydata, pch=20, cex=0.75, col="red", add=TRUE)
+  
+  cat(paste("The bandwidth for the kernel density is"))
+  cat(paste(kde$bandwidth))
 
-
-# plotting the map with some points on i
-ggmap(mapgilbert) +
-  geom_point(data = df[1:100,], aes(x = decimalLongitude, y = decimalLatitude, alpha = 0.5, size = name, colour= name ),shape=20) +
-  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+  
+  #CoordinatePrecision & coordinateuncertainityinmeters
+  if(!is.null(COORDINATEPRECISION)){
+    XCP<-subset(X,X$coordinatePrecision<COORDINATEPRECISION)
+    if(nrow(XCP)==0){
+      cat(paste("There are no observations with coordinate precision greater than ",COORDINATEPRECISION))
+    }
+    else{
+    c_1<-ddply(XCP,~coordinatePrecision,summarise,number_of_distinct_orders=length((coordinatePrecision)))
+    
+    #plot_ly(c_1, x= ~coordinatePrecision, y= ~number_of_distinct_orders,type="bar")
+    htmlTable::htmlTable(c_1)
+    }
+  }
+  else{
+  c_1<-ddply(X,~coordinatePrecision,summarise,number_of_distinct_orders=length((coordinatePrecision)))
+  
+  #plot_ly(c_1, x= ~coordinatePrecision, y= ~number_of_distinct_orders,type="bar")
+  htmlTable::htmlTable(c_1)
+  v1<-max(c_1[,2])
+  cat(paste("The maximum value for coordinatePrecision is",v1))
+  c_1[c_1$number_of_distinct_orders==v1,]
+  
+  v2<-min(c_1[,2])
+  cat(paste("The minimum value for coordinatePrecision is",v2))
+  c_1[c_1$number_of_distinct_orders==v2,]
+  }
+  
+  if(!is.null(COORDINATEUNCERTAINITYINMETERS)){
+    XCP<-subset(X,X$coordinateUncertaintyInMeters<COORDINATEUNCERTAINITYINMETERS)
+    if(nrow(XCP)==0){
+      cat(paste("There are no observations with COORDINATE UNCERTAINITY IN METERS less than ",COORDINATEUNCERTAINITYINMETERS))
+    }
+    else{
+    c_2<-ddply(XCP,~coordinateUncertaintyInMeters,summarise,number_of_distinct_orders=length((coordinateUncertaintyInMeters)))
+    
+    #plot_ly(c_1, x= ~coordinatePrecision, y= ~number_of_distinct_orders,type="bar")
+    htmlTable::htmlTable(c_2)
+    }
+  }
+  else{
+  c_2<-ddply(X,~coordinateUncertaintyInMeters,summarise,number_of_distinct_orders=length((coordinateUncertaintyInMeters)))
+  #plot_ly(c_2, x= ~coordinateUncertaintyInMeters, y= ~number_of_distinct_orders,type="bar")
+  htmlTable::htmlTable(c_2)
+  v1<-max(c_2[,2])
+  cat(paste("The maximum value for coordinateUncertaintyInMeters is",v1))
+  c_2[c_2$number_of_distinct_orders==v1,]
+  
+  v2<-min(c_2[,2])
+  cat(paste("The minimum value for coordinateUncertaintyInMeters is",v2))
+  c_2[c_2$number_of_distinct_orders==v2,]
+  }
+  
+  #View(X)
+  }
+  
+}
