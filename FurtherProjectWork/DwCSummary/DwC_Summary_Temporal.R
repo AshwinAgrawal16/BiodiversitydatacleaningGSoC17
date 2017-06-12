dd6b5df0c4b5f9a39d634137835cd5c0c7de6a5a
Ashwin Agrawal <ashwin.agrawal.met14@itbhu.ac.in>
2017-06-12 21:16:41 +0530

    Small changes

--- FurtherProjectWork/DwCSummary/DwC_Summary_Temporal.R
+++ FurtherProjectWork/DwCSummary/DwC_Summary_Temporal.R
@@ -1,38 +1,55 @@
 DwC_Summary_Temporal<-function(X,DATESTART=NULL,
-                               DATEEND=NULL
+                               DATEEND=NULL,
                                MONTH=NULL,
                                YEAR=NULL){
   
-  
+
   X<-subset(X,select=c(eventDate,month,year))
+
   
-  if(is.null(nrow(X))){
-    cat(paste("The data set is empty"))
+  if((nrow(X))==0){
+    stop(sprintf("The data set is empty"))
   }
   else{
     
-   if((!is.null(DATESTART) && !is.null(DATEEND)) || !is.null(MONTH) || !is.null(nrow(YEAR)) ){
+   if(((!is.null(DATESTART) && !is.null(DATEEND)) || !is.null(MONTH) || !is.null((YEAR)) )){
     
-    if(!is.null(DATESTART) && !is.null(DATEEND)){
+    if((!is.null(DATESTART) && !is.null(DATEEND))){
+       d1<- try( as.Date( DATESTART, format= "%Y-%m-%d %H:%M:%S" ) )
+      d2<- try( as.Date( DATEEND , format= "%Y-%m-%d %H:%M:%S" ) )
       
-      
-      if(is.null(nrow(X))){
-        stop(sprintf("There are no observations between the specified date"))
+      if( class(d1) == "try-error" || is.na( d1 ) || class( d2 ) == "try-error" || is.na(d2)  ){
+        stop(sprintf("Please enter the date in %Y-%m-%d %H:%M:%S format"))
+      } 
+      else{
+        DATE1 <- as.Date(DATESTART)
+        DATE2 <- as.Date(DATEEND)
+        X<-X[X$eventDate>=DATE1 && X$eventDate<=DATE2,]
+        
       }
-    }
+      if((nrow(X))==0){
+        stop(sprintf("There are no observations between the specified date"))
+        }
+      }
+    
     if(!is.null(MONTH)){
-      X<-subset(X,month=MONTH)
+      X<-subset(X,month==MONTH)
       
-      if(is.null(nrow(X))){
+      if(MONTH>12 || MONTH<1){
+        stop(sprintf("The month is wrong"))
+      }
+      
+      if((nrow(X))==0){
         stop(sprintf("There are no observations in the specified month"))
       }
     }
      
-    if(!is.null(nrow(YEAR))){
-      X<-subset(X,year=YEAR)
+    if(!is.null((YEAR))){
+      X<-subset(X,year==YEAR)
       
-      if(is.null(nrow(YEAR))){
+      if((nrow(X))==0){
         stop(sprintf(("There are no observations in this year")))
+        
       }
     }
      
@@ -43,9 +60,9 @@ DwC_Summary_Temporal<-function(X,DATESTART=NULL,
      
      if(is.null(MONTH)){
        c_1<-ddply(X,~month,summarise,number_of_distinct_orders=length((month)))
-       plot_ly(c_1, x= ~month, y= ~number_of_distinct_orders,type="bar")
-       tempolar(X1)
        
+       tempolar(X1)
+       plot_ly(c_1, x= ~month, y= ~number_of_distinct_orders,type="bar")
      }
      
      if(is.null(YEAR)){
@@ -58,14 +75,38 @@ DwC_Summary_Temporal<-function(X,DATESTART=NULL,
        }
      }
      
-     
-     
-     
-     
    }
     
     else{
+      X1<-X
+      names(X1)[names(X1) == "eventDate"] <- "Date_collected"
+      chronohorogram(X1)
       
+      c_1<-ddply(X,~month,summarise,number_of_distinct_orders=length((month)))
+      plot_ly(c_1, x= ~month, y= ~number_of_distinct_orders,type="bar")
+      tempolar(X1)
+      
+      c_2<-ddply(X,~year,summarise,number_of_distinct_orders=length((year)))
+      if(nrow(c_2)>10){
+        htmlTable::htmlTable(c_2)
+      }
+      else{
+        plot_ly(c_2, x= ~year, y= ~number_of_distinct_orders,type="bar")
+      }
     }
   }  
 }
+
+#EXAMPLE
+
+library(rgbif)
+library(plyr)
+library(htmlTable)
+library(bdvis)
+library(plotly)
+
+DwC_Summary_Temporal(X,MONTH = 0)
+DwC_Summary_Temporal(X,MONTH = 2)
+DwC_Summary_Temporal(X,YEAR = 2017)
+DwC_Summary_Temporal(X,YEAR = 2018)
+DwC_Summary_Temporal(X)
